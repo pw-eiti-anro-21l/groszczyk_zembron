6de7d2a393ba1060884b46760de579b606355bae
MaciejGroszyk <maciekgroszyk12@gmail.com>
2021-05-19 22:39:45 +0200

    przeskakuje, ale niby nie powinno

--- zad4/zad4/ikin.py
+++ zad4/zad4/ikin.py
@@ -8,8 +8,9 @@ from rclpy.qos import QoSProfile
 import time
 
 from scipy.optimize import fsolve
-from math import sin, cos
+from math import  pi, sin, cos
 import numpy as np
+from sympy import Symbol, solve, sin, cos, Eq, Matrix, nsolve
 
 
 base_y=0.2
@@ -18,23 +19,23 @@ element2_param=[0.2, 0.2, 0.2]
 element3_param=[0.2, 0.2, 0.6]
 element4_param=[]
 element_param=[element1_param, element2_param, element3_param]
-filepath='parametryDH.txt'
+
 
 
 class ikin(Node):
 	
 	def __init__(self):
 		super().__init__('ikin')
+		self.limit=(pi/2)*0.5
 		self.sphere_center=[0, 0, element1_param[2]+base_y]
 		self.sphere_radius=4
 		self.subscription = self.create_subscription(PoseStamped, '/oint_interpolate', self.listener_callback, 10)
 
-		# self.dataDH=[['r1'], ['r2'], ['r3']]
-		# self.readDHParameters(filepath)
 
 		#self.subscription #prevent unused variable waring
 		qos_profile = QoSProfile(depth=10)
 		self.publisher = self.create_publisher(JointState, 'joint_interpolate', qos_profile)
+		self.previous_joint_values=[0, 0, 0]
 
 		
 
@@ -51,16 +52,68 @@ class ikin(Node):
 		#self.get_logger().info(str(self.dataDH))
 
 
-		theta= fsolve(self.f, [1.0, 1.0, 1.0])
-		time_interval = 0.1
 		
+		i=0;
+		v1=self.previous_joint_values[0]
+		v2=self.previous_joint_values[1]
+		v3=self.previous_joint_values[2]
+
+		theta= fsolve(self.f, [v1, v2, v3])
+		theta2= fsolve(self.f2, [v1, v2, v3])
+
+		theta[0]=float(theta[0])%(2*pi)
+		theta[0]=self.from_minus_pi_to_plus_pi(theta[0])
+
+		theta[1]=float(theta[1])%(2*pi)
+		theta[1]=self.from_minus_pi_to_plus_pi(theta[1])
+
+		theta[2]=float(theta[2])%(2*pi)
+		theta[2]=self.from_minus_pi_to_plus_pi(theta[2])
+
+
+		theta2[0]=float(theta2[0])%(2*pi)
+		theta2[0]=self.from_minus_pi_to_plus_pi(theta2[0])
+
+		theta2[1]=float(theta2[1])%(2*pi)
+		theta2[1]=self.from_minus_pi_to_plus_pi(theta2[1])
+
+		theta2[2]=float(theta[2])%(2*pi)*-1
+		theta2[2]=self.from_minus_pi_to_plus_pi(theta2[2])
+
+		
+		if abs(theta[1]-self.previous_joint_values[1])<=abs(theta2[1]-self.previous_joint_values[1]):
+			solution=theta
+		else:
+			solution=theta2
+
+		
+			
+		
+		
+
+
+		#time_interval = 0.1
+		
+
 		joint_states = JointState()
 		joint_states.name = ['joint_base_element1', 'joint_element1_element2', 'joint_element2_element3']
-		joint_states.position=[float(theta[0]), float(theta[1]), float(theta[2])]
+		
+		
+		joint1_value=solution[0]		
+		joint2_value=solution[1]
+		joint3_value=solution[2]
+		
+		joint_states.position=[joint1_value, joint2_value, joint3_value]
 
+
+		self.previous_joint_values[0]=joint1_value
+		self.previous_joint_values[1]=joint2_value
+		self.previous_joint_values[2]=joint3_value
 		self.publisher.publish(joint_states)
-		self.get_logger().info(str(theta))
-		time.sleep(time_interval)
+
+		self.get_logger().info(str(theta)+" "+str(theta2))
+		
+		#time.sleep(time_interval)
 
 
 	def point_in_sphere(self, x, y, z):
@@ -69,21 +122,15 @@ class ikin(Node):
 		else:
 			return True
 
+	def from_minus_pi_to_plus_pi(self, value):
+		if value>pi:
+			value= value- 2*pi
+		if value<-pi:
+			value= value+ 2*pi
+		return value
 
 
-	# def readDHParameters(self, plik):
-	# 	i=0
-	# 	if os.path.isfile(plik):
-	# 		with open(plik, "r") as f:
-	# 			for line in f:
-	# 				line=line.strip('\n') 
-	# 				line=line.split(" ")
-	# 				self.dataDH[i].append(line)
-	# 				i+=1
-	# 		f.close()		
-		
-	# 	else:
-	# 		print("Błąd odczytu danych!")
+
 
 	def f(self, x):
 		theta1= x[0]
@@ -123,6 +170,47 @@ class ikin(Node):
 		f[2]= T[2][3] -self.position_z+ self.sphere_center[2]
 
 		return f
+
+	def f2(self, x):
+		theta1= x[0]
+		theta2= x[1]
+		p=x[2]
+		theta3= -self.previous_joint_values[2]
+		l2=2
+		l3=2
+		T1 = np.array([
+			[cos(theta1), -sin(theta1), 0, 0],
+			[sin(theta1), cos(theta1), 0, 0],
+			[0*p, 0, 1, 0],
+			[0, 0, 0, 1]
+			])
+		T2 = np.array([
+			[cos(theta2), -sin(theta2), 0, 0],
+			[0, 0, 1, 0],
+			[-sin(theta2), -cos(theta2), 0, 0],
+			[0, 0, 0, 1]
+			])
+		T3 = np.array([
+			[cos(theta3), -sin(theta3), 0, l2],
+			[sin(theta3), cos(theta3), 0, 0],
+			[0, 0, 1, 0],
+			[0, 0, 0, 1]
+			])
+		T4 = np.array([
+			[1, 0, 0, l3],
+			[0, 1, 0, 0],
+			[0, 0, 1, 0],
+			[0, 0, 0, 1]
+			])
+		T= T1 @ T2 @ T3@ T4
+
+		f=np.zeros(3)
+		f[0]= T[0][3] -self.position_x
+		f[1]= T[1][3] -self.position_y
+		f[2]= T[2][3] -self.position_z+ self.sphere_center[2]
+
+		return f
+
 
 
 def main(args=None):
