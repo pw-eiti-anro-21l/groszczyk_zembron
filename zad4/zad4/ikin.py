b830be6086fb0637edd4d6053a960fe3f3fe6097
MaciejGroszyk <maciekgroszyk12@gmail.com>
2021-05-20 12:46:12 +0200

    dziala poprawnie

--- zad4/zad4/ikin.py
+++ zad4/zad4/ikin.py
@@ -36,6 +36,7 @@ class ikin(Node):
 		qos_profile = QoSProfile(depth=10)
 		self.publisher = self.create_publisher(JointState, 'joint_interpolate', qos_profile)
 		self.previous_joint_values=[0, 0, 0]
+		self.last_corret_position=[4, 0, 1.9]
 
 		
 
@@ -47,78 +48,50 @@ class ikin(Node):
 		self.position_z= msg.pose.position.z
 		self.position= [self.position_x, self.position_y, self.position_z]
 
+		positionCorrect=self.point_in_sphere(self.position_x, self.position_y, self.position_z)
 
-		bool=self.point_in_sphere(self.position_x, self.position_y, self.position_z)
-		#self.get_logger().info(str(self.dataDH))
-
-
-		
-		i=0;
+		#przypisane wartości stawów do krótszej nazwy
 		v1=self.previous_joint_values[0]
 		v2=self.previous_joint_values[1]
 		v3=self.previous_joint_values[2]
 
-		theta= fsolve(self.f, [v1, v2, v3])
-		theta2= fsolve(self.f2, [v1, v2, v3])
+		if positionCorrect:
+			self.last_corret_position=self.position
+			theta= fsolve(self.f, [v1, v2, v3], [1, 1, 1, 0, 0, 0])
+			theta2= fsolve(self.f, [v1, v2, v3], [1, 1, 0, 0, 0, -v3])
 
-		theta[0]=float(theta[0])%(2*pi)
-		theta[0]=self.from_minus_pi_to_plus_pi(theta[0])
+			theta= self.correct_the_angles(theta)
+			theta2= self.correct_the_angles(theta2)
 
-		theta[1]=float(theta[1])%(2*pi)
-		theta[1]=self.from_minus_pi_to_plus_pi(theta[1])
+			if self.position_x==0 and self.position_y==0:
+				theta[0]=self.previous_joint_values[0]
+				theta2[0]= self.previous_joint_values[1]
 
-		theta[2]=float(theta[2])%(2*pi)
-		theta[2]=self.from_minus_pi_to_plus_pi(theta[2])
-
-
-		theta2[0]=float(theta[0])%(2*pi)
-		theta2[0]=self.from_minus_pi_to_plus_pi(theta2[0])
-
-		theta2[1]=float(theta2[1])%(2*pi)
-		theta2[1]=self.from_minus_pi_to_plus_pi(theta2[1])
-
-		theta2[2]=float(theta[2])%(2*pi)*-1
-		theta2[2]=self.from_minus_pi_to_plus_pi(theta2[2])
-
-		if self.position_x==0 and self.position_y==0:
-			theta[0]=self.previous_joint_values[0]
-			theta2[0]= self.previous_joint_values[1]
-
-		
-		if abs(theta[1]-self.previous_joint_values[1])<=abs(theta2[1]-self.previous_joint_values[1]):
-			solution=theta
-		else:
-			solution=theta2
-
-		
 			
-		
-		
+			if abs(theta[1]-self.previous_joint_values[1])<=abs(theta2[1]-self.previous_joint_values[1]):
+				solution=theta
+			else:
+				solution=theta
+
+			joint_states = JointState()
+			joint_states.name = ['joint_base_element1', 'joint_element1_element2', 'joint_element2_element3']
+			
+			
+			joint1_value=solution[0]		
+			joint2_value=solution[1]
+			joint3_value=solution[2]
+			
+			joint_states.position=[joint1_value, joint2_value, joint3_value]
 
 
-		
-		
+			self.previous_joint_values[0]=joint1_value
+			self.previous_joint_values[1]=joint2_value
+			self.previous_joint_values[2]=joint3_value
+			self.publisher.publish(joint_states)
 
-		joint_states = JointState()
-		joint_states.name = ['joint_base_element1', 'joint_element1_element2', 'joint_element2_element3']
-		
-		
-		joint1_value=solution[0]		
-		joint2_value=solution[1]
-		joint3_value=solution[2]
-		
-		joint_states.position=[joint1_value, joint2_value, joint3_value]
-
-
-		self.previous_joint_values[0]=joint1_value
-		self.previous_joint_values[1]=joint2_value
-		self.previous_joint_values[2]=joint3_value
-		self.publisher.publish(joint_states)
-
-		self.get_logger().info(str(theta)+" "+str(theta2))
-		
-		
-
+			self.get_logger().info(str(theta)+" "+str(theta2))
+		else:
+			self.get_logger().info("Zadany punkt jest nieprawidłowy. Ostatni prawidłowy punkt to: "+ str(self.last_corret_position))
 
 	def point_in_sphere(self, x, y, z):
 		if (x-self.sphere_center[0])**2+(y-self.sphere_center[1])**2+(z-self.sphere_center[2])**2>self.sphere_radius**2:
@@ -133,20 +106,44 @@ class ikin(Node):
 			value= value+ 2*pi
 		return value
 
+	def correct_the_angles(self, values):
+		new_values=[]
+		for value in values:
+			value= float(value% (2*pi))
+			value= self.from_minus_pi_to_plus_pi(value)
+			new_values.append(value)
+		return new_values
 
 
+	def f(self, x, args):
+		if args[0]==1:
+			theta1= x[0]
+			p1=0
+		else:
+			theta1=args[3]
+			p1=x[0]
 
-	def f(self, x):
-		theta1= x[0]
-		theta2= x[1]
-		theta3= x[2]
+		if args[1]==1:
+			theta2= x[1]
+			p2=0
+		else:
+			theta2=args[4]
+			p2=x[1]
+
+		if args[2]==1:
+			theta3= x[2]
+			p3=0
+		else:
+			theta3=args[5]
+			p3=x[2]
+
 		l2=2
 		l3=2
 		T1 = np.array([
 			[cos(theta1), -sin(theta1), 0, 0],
 			[sin(theta1), cos(theta1), 0, 0],
 			[0, 0, 1, 0],
-			[0, 0, 0, 1]
+			[0*p1*p2*p3, 0, 0, 1]
 			])
 		T2 = np.array([
 			[cos(theta2), -sin(theta2), 0, 0],
@@ -174,48 +171,6 @@ class ikin(Node):
 		f[2]= T[2][3] -self.position_z+ self.sphere_center[2]
 
 		return f
-
-	def f2(self, x):
-		theta1= self.previous_joint_values[0]
-		p0=x[0]
-		theta2= x[1]
-		p=x[2]
-		theta3= -self.previous_joint_values[2]
-		l2=2
-		l3=2
-		T1 = np.array([
-			[cos(theta1), -sin(theta1), 0, 0],
-			[sin(theta1), cos(theta1), 0, 0],
-			[0*p, 0*p0, 1, 0],
-			[0, 0, 0, 1]
-			])
-		T2 = np.array([
-			[cos(theta2), -sin(theta2), 0, 0],
-			[0, 0, 1, 0],
-			[-sin(theta2), -cos(theta2), 0, 0],
-			[0, 0, 0, 1]
-			])
-		T3 = np.array([
-			[cos(theta3), -sin(theta3), 0, l2],
-			[sin(theta3), cos(theta3), 0, 0],
-			[0, 0, 1, 0],
-			[0, 0, 0, 1]
-			])
-		T4 = np.array([
-			[1, 0, 0, l3],
-			[0, 1, 0, 0],
-			[0, 0, 1, 0],
-			[0, 0, 0, 1]
-			])
-		T= T1 @ T2 @ T3@ T4
-
-		f=np.zeros(3)
-		f[0]= T[0][3] -self.position_x
-		f[1]= T[1][3] -self.position_y
-		f[2]= T[2][3] -self.position_z+ self.sphere_center[2]
-
-		return f
-
 
 
 def main(args=None):
